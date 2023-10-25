b50ebe0febf6d3bdbfb9f8cb927559b5901a3dc6
Elishiba Muigo <elishibamuthoni3@gmail.com>
2023-10-25 22:04:00 +0300

    first commit

---
+++ OOB.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self,name,age,ID,GPA):
+        self.name=name
+        self.age=age
+        self.ID=ID
+        self.GPA=GPA
+
+stud_1=Student("Jane",21,283738,3.2)
+stud_2=Student("Mike",20,324233,2.8)
+
+#print(stud_1)
+#print(stud_2)
+
+print(stud_1.name)
+print(stud_2.name)
+    
